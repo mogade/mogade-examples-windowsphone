c12ce80ca4ee7d7742181b210f304f080cafa9c8
Karl Seguin <karl@openmymind.net>
2011-06-27 21:42:11 +0800

    point to a real game/leaderboard

--- Mogade.Examples.WindowsPhone/Code/MogadeHelper.cs
+++ Mogade.Examples.WindowsPhone/Code/MogadeHelper.cs
@@ -10,18 +10,11 @@ namespace Mogade.Examples.WindowsPhone
    public class MogadeHelper
    {
       //Your game key and game secret
-//      private const string _gameKey = "4d6a4bf6bc7a05710a000011";
-//      private const string _secret = "<_nnqvisX0T5L\\oTFLAcaCTY";
-//      private static readonly IDictionary<Leaderboards, string> _leaderboardLookup = new Dictionary<Leaderboards, string>
-//                                                                                     {
-//                                                                                        {Leaderboards.Main, "4d6a4c32bc7a05710a000012"}
-//                                                                                     };
-
-      private const string _gameKey = "4d32a48b1d95175345000003";
-      private const string _secret = "2AieOv9k:MZ]y@FGgo1WBQ6A;g\\>7Drp";
+      private const string _gameKey = "4e0888448fcd52202500000a";
+      private const string _secret = "B:lkvrDb>AVmaR:Jwh:JqSdRDHy>K@1:Vrh";
       private static readonly IDictionary<Leaderboards, string> _leaderboardLookup = new Dictionary<Leaderboards, string>
                                                                                            {
-                                                                                              {Leaderboards.Main, "4d5f933e563d8a2e9900001b"}
+                                                                                              {Leaderboards.Main, "4e08885d8fcd52202500000b"}
                                                                                            };
 
       public static string LeaderboardId(Leaderboards leaderboard)
@@ -33,7 +26,7 @@ namespace Mogade.Examples.WindowsPhone
       {
          //In your own game, when you are ready, REMOVE the ContectToTest to hit the production mogade server (instead of testing.mogade.com)
          //Also, if you are upgrading from the v1 library and you were using UserId (or not doing anything), you *must* change the UniqueIdStrategy to LegacyUserId
-         MogadeConfiguration.Configuration(c => c.UsingUniqueIdStrategy(UniqueIdStrategy.UserId).ConnectTo("http://192.168.1.102:3000/api/"));
+         MogadeConfiguration.Configuration(c => c.UsingUniqueIdStrategy(UniqueIdStrategy.UserId));
          return MogadeClient.Initialize(_gameKey, _secret);
       }
    }
